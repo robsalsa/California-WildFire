0c9ee5ab2209972af1314a2b818959af9954c16a
Robert <robertosalazarvasquezalexis@gmail.com>
2025-05-21 11:36:50 -0700

    Comments on Sections for Other Users
    This is mainly for the Frontend team to have an easier time reading and programing in react.

--- cali-wildfire-front/src/parts/HomeSection/Home.js
+++ cali-wildfire-front/src/parts/HomeSection/Home.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-
+//these are nessary imports for a simple page. Just have those three things and youll be fine
+//make sure the "./(Name of CSS file)" is correct or else the page will refuse to work
 
 export const Home = () => {
     return (
         <div className="home-container">
             <section id="notification-bar" className="notification-bar">
                 <h1>Cali-Wildfire Watch</h1>
-                <nav>
+                {/* <nav>
                     <button id="placeholder">Placeholder1</button>
                     <button id="placeholder2">Placeholder2</button>
-                </nav>
+                </nav> */}
             </section>
+{/* Section ID: Notification-Bar ClassName: Notification-Bar 
+	this is just the title name. I was thinking to use both the burger 
+    stack and some links but mine broke soooo yeah.  */}
+
 
             <main className="content-wrapper">
                 <section id="box-container" className="box-container">
@@ -35,6 +40,18 @@ export const Home = () => {
                     </section>
                 </aside>
             </main>
+
+{/* main ClassName: Content Wrapper 
+	is the actual look of the page
+
+	Section ID: Box Container ClassName: Box Container
+		is the MAIN PAGE!!! This means that the actual page elements, components, and features are all going to end up here!
+		
+		NOTE: Either place your map API inside of Section ID: Fire-Map-View or inside of Div ID: Actual Map. 
+
+	Aside ID: Sidebar 
+		Is exactly as the name means its the actual side bar.  */}
+
         </div>
     );
 };
