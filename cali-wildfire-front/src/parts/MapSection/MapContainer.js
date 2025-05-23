6668d7b14c3f93ddc8df50f059b02df0c534d05a
Robert <robertosalazarvasquezalexis@gmail.com>
2025-05-23 11:18:02 -0700

    Map Pins active and Working Due to A CSV

--- cali-wildfire-front/src/parts/MapSection/MapContainer.js
+++ cali-wildfire-front/src/parts/MapSection/MapContainer.js
@@ -1,36 +1,90 @@
-import React from "react";
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-
-// request('dotenv').config();  i guess i can go screw myself huh
+import React, { useState, useEffect } from "react";
+import {GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import Papa from "papaparse";
 
 const containerStyle ={
-    width: '100%',
+    width:'100%',
     height: '500px'
 };
 
-// this might not be needed... I think 
-
 const center ={
-    lat: 34.009242,
-    lng: -118.497604
+    lat:34.009242,
+    lng:-118.497604
+};
 
-}
+function MapContainer(){
+    const [ubicacion, setUbicacion] =useState([]);
 
-function MapContainer() {
+    useEffect(() => {
+        fetch("/mapdataall.csv")
+        .then((response) => response.text())
+        .then((csvText) => {
+            const parsed=Papa.parse(csvText, {header: true});
+            setUbicacion(parsed.data);
+        });
+    }, []);
+
     return (
-        <LoadScript googleMapApiKey={process.env.REACT_APP_GOOGLE_MAP_API}>
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={10}
-            >
-                {/* Extra shit goes here: Pins, Labels, Pop-ups */}
+        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API}>
+            <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+                {ubicacion.map((location, index) =>{
+                    const lat = parseFloat(location.incident_latitude);
+                    const lng = parseFloat(location.incident_longitude);
+                    if(isNaN(lat) || isNaN(lng)) return null;
+
+                    return(
+                        <Marker
+                            key={index}
+                            position={{lat,lng}}
+                            title={location.incident_name}
+                            // info={location.incident_url}
+                        />
+                    )
+                })}
             </GoogleMap>
         </LoadScript>
     );
 }
 
 export default MapContainer;
+
+
+
+
+
+// import React from "react";
+// import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+// // request('dotenv').config();  i guess i can go screw myself huh
+
+// const containerStyle ={
+//     width: '100%',
+//     height: '500px'
+// };
+
+// // this might not be needed... I think 
+
+// const center ={
+//     lat: 34.009242,
+//     lng: -118.497604
+
+// }
+
+// function MapContainer() {
+//     return (
+//         <LoadScript googleMapApiKey={process.env.REACT_APP_GOOGLE_MAP_API}>
+//             <GoogleMap
+//                 mapContainerStyle={containerStyle}
+//                 center={center}
+//                 zoom={10}
+//             >
+//                 {/* Extra shit goes here: Pins, Labels, Pop-ups */}
+//             </GoogleMap>
+//         </LoadScript>
+//     );
+// }
+
+// export default MapContainer;
 
 
 
